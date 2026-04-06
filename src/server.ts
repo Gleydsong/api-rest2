7ae6c5c6ea2d19aa21c6f12be28ae841844e90a8
Gleydsong <ggleydson@icloud.com>
2026-04-07 00:23:01 +0100

    feat: add supertest for integration testing and configure VS Code formatting settings

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,6 @@
-import cookie from "@fastify/cookie";
-import fastify from "fastify";
+import { app } from './app.js'
 import { env } from "./env/index.js";
-import { transactionsRoutes } from "./routes/transactions.js";
 
-const app = fastify();
-
-app.register(cookie);
-
-app.addHook("preHandler", async (request, reply) => {
-  console.log(`${request.method} ${request.url}`);
-});
-
-app.register(transactionsRoutes, {
-  prefix: "/transactions",
-});
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server is running on port ${env.PORT}`);
