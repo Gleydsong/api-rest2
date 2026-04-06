57f6f4211c09cc2f322e09d5e61266036a1c3351
Gleydsong <ggleydson@icloud.com>
2026-04-06 01:31:21 +0100

    feat: integrate @fastify/cookie for session management and add transaction retrieval endpoints

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,27 @@
 import type { FastifyInstance } from "fastify";
+import { knex } from "../database.js";
 import crypto from "node:crypto";
 import { z } from "zod";
-import { knex } from "../database.js";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async (request, reply) => {
+    const transactions = await knex("transactions").select();
+
+    return { transactions };
+  });
+
+  app.get("/:id", async (request, reply) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionsParamsSchema.parse(request.params);
+
+    const transaction = await knex("transactions").where("id", id).first();
+
+    return { transaction };
+  });
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -15,31 +33,24 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     );
 
-    let sessionId = request.headers.cookie
-      ?.split(";")
-      .map((item) => item.trim())
-      .find((item) => item.startsWith("sessionId="))
-      ?.split("=")[1];
+    let sessionId = request.cookies.sessionId;
 
     if (!sessionId) {
       sessionId = crypto.randomUUID();
 
-      reply.header("Set-Cookie", `sessionId=${sessionId}; Path=/; HttpOnly`);
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
     }
 
     const transaction = await knex("transactions").insert({
       id: crypto.randomUUID(),
-      session_id: sessionId,
       title,
       amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId,
     });
 
-    return reply.status(201).send(transaction);
-  });
-
-  app.get("/transactions", async (request, reply) => {
-    const transactions = await knex("transactions").select("*");
-
-    return reply.status(200).send(transactions);
+    return reply.status(201).send();
   });
 }
